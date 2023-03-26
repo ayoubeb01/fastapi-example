324ef2c7746dc3785060b65e03f27305329da2a0
ayoube <aybuchi@gmail.com>
2023-03-26 20:08:03 +0200

    add proc file

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World ayoube bakhouch this is your first fastapiapplication on Heroku"}
+    return {"message": "Hello World ayoube bakhouch this is your first fastapiapplication on Heroku "}
 
 
 
