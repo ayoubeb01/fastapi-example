124eca2bbb608d22bf4107853223ea2052d5d2c6
ayoube <aybuchi@gmail.com>
2023-04-15 07:45:35 +0200

    password fixes

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World ayoube bakhouch this is a ci/cd"}
+    return {"message": "Hello World ayoube bakhouch this is a ci/cd to ubuntu server"}
 
 
 
